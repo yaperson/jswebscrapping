0be4a62d2254e72e4a1ebdd2341ed24f6a22a2b8
yaperson <71440841+yaperson@users.noreply.github.com>
2023-03-03 16:05:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://sites.weezevent.com/mergueztuningshow/');
 
-    const soldOut = await page.waitForSelector('div > .soldOut');
+    const soldOut = await page.waitForSelector('tr > .soldOut');
 
     console.log(soldOut);
 })();
