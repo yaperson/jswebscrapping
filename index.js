cbd3c5ccac1c76a1aabc055cea80bfbb5f99c158
yanis <yanis.legrand.1@gmail.com>
2023-03-08 10:27:58 +0100

    scrap changelog - WP Rocket

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://sites.weezevent.com/mergueztuningshow/');
+    await page.tracing.start({
+        path: 'trace.json',
+        categories: ['devtools.timeline']
+    })
+    await page.goto('https://wp-rocket.me/changelog/');
 
-    const soldOut = await page.waitForSelector('tr > .soldOut');
+    const title = await page.title()
 
-    console.log(soldOut);
+    const name = await page.$eval('h2', el => el.innerText);
+
+    const stories = await page.$$eval('h2', anchors => { 
+        return anchors.map(anchor => anchor.textContent).slice(0, 10) 
+    })
+
+    console.log(stories)
+    console.log('last data for ' + title + ' : ' + name);
+
+    await page.tracing.stop()
+    await browser.close();
 })();
