7c72625911882b6bc159cebce923d4cf87af3ff9
yanis <yanis.legrand.1@gmail.com>
2023-03-27 11:20:14 +0200

    codeforgeek

--- index.js
+++ index.js
@@ -7,17 +7,51 @@ const puppeteer = require('puppeteer');
         path: 'trace.json',
         categories: ['devtools.timeline']
     })
-    await page.goto('https://wp-rocket.me/changelog/');
+    await page.goto('https://codeforgeek.com/');
 
     const title = await page.title()
 
     const name = await page.$eval('h2', el => el.innerText);
 
-    const stories = await page.$$eval('h2', anchors => { 
-        return anchors.map(anchor => anchor.textContent).slice(0, 10) 
+    const stories = await page.$$eval('article > div > ul', anchors => {
+        return anchors.map(anchor => anchor.textContent).slice(0, 10)
     })
 
-    console.log(stories)
+    const articles = await page.$$eval('article > div', anchors => {
+        let tag = anchors.map(anchor =>
+            anchor = anchor.getElementsByTagName('li')
+        )
+        let link = anchors.map(anchor =>
+            anchor.getElementsByTagName('a')
+        )
+        
+        // elements[elem][data]
+        tag = tag[0][2].innerText // 0 -> tag, 1 -> author, 2 -> date
+        link = link[0][2].getAttribute('href') // 0 -> tag link, 1 -> article link, 2 -> author link
+
+        return { tag, link }
+    })
+
+    const linkArray = await page.$$eval('article > div > h2 > a', anchors => {
+        let link = anchors.map(anchor =>
+            anchor.getAttribute('href')
+        )
+        let text = anchors.map(anchor =>
+            anchor.textContent
+        )
+        return { link, text }
+    })
+
+    // const linkArray2 = await page.evaluate(
+    //     () => Array.from(
+    //       document.querySelectorAll('a[href]'),
+    //       a => a.getAttribute('href'), a.text()
+    //     )
+    //   );
+
+
+    console.log(articles)
+    // console.log(linkArray)
     console.log('last data for ' + title + ' : ' + name);
 
     await page.tracing.stop()
